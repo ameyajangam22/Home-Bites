a4441518ee7fefca67b89c234cda28ac92f8453e
ameyajangam22 <ameya.g.jangam@gmail.com>
2021-07-18 02:16:47 +0530

    intiial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				primary: "#ffa500",
+				secondary: {
+					100: "#E2E2D5",
+					200: "#888883",
+				},
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
