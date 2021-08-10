3a4e0b4cc43e80f34b1bd1c9c105be19a89d2205
ameyajangam22 <ameya.g.jangam@gmail.com>
2021-08-10 14:25:58 +0530

    Made User Navbar responsive

--- src/components/User/Carousel.js
+++ src/components/User/Carousel.js
@@ -17,7 +17,7 @@ SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay]);
 const Carousel = () => {
 	return (
 		<>
-			<div className="bg-gray-800 m-auto text-white text-5xl justify-center h-48 md:h-xl md:text-8xl flex">
+			<div className="bg-gray-900 m-auto text-white text-5xl justify-center h-52 md:h-xl md:text-8xl flex">
 				<Swiper
 					spaceBetween={0}
 					slidesPerView={1}
@@ -29,19 +29,21 @@ const Carousel = () => {
 				>
 					<SwiperSlide className="flex cursor-pointer justify-center items-center">
 						<img
-							className=""
+							className="h-full"
 							src="https://res.cloudinary.com/home-bites/image/upload/v1628575575/slide1-01_hi0dpp.jpg"
 							alt="slide-1"
 						/>
 					</SwiperSlide>
 					<SwiperSlide className="flex cursor-pointer justify-center items-center">
 						<img
+							className="h-full"
 							src="https://res.cloudinary.com/home-bites/image/upload/v1628575575/slide2-01_wb9uwe.jpg"
 							alt="Slide-2"
 						/>
 					</SwiperSlide>
 					<SwiperSlide className="flex cursor-pointer justify-center items-center">
 						<img
+							className="h-full"
 							src="https://res.cloudinary.com/home-bites/image/upload/v1628576571/slide-3-01_ryvkql.jpg"
 							alt="Slide-3"
 						/>
--- src/components/User/Navbar.js
+++ src/components/User/Navbar.js
@@ -8,6 +8,7 @@ import { ReactComponent as HamBurgerIcon } from "../../icons/hamBurger.svg";
 import { ReactComponent as ThunderIcon } from "../../icons/thunder.svg";
 import { Link, useHistory } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { slide as Menu } from "react-burger-menu";
 import Modal from "../Common/Modal";
 const Navbar = ({ type, pg, cartCount }) => {
 	const history = useHistory();
@@ -15,7 +16,11 @@ const Navbar = ({ type, pg, cartCount }) => {
 	const [userName, setUserName] = useState("Anon");
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
 	const [showLinks, setShowLinks] = useState(true);
+	const [menuOpen, setMenuOpen] = useState(false);
 	const [sellerLogin, setSellerLogin] = useState(false);
+	const [showSignIn, setShowSignIn] = useState(true);
+	const [showLogout, setShowLogout] = useState(false);
+	const [showMyOrders, setShowMyOrders] = useState(false);
 	useEffect(() => {
 		if (type === "seller") {
 			setShowLinks(false);
@@ -41,115 +46,200 @@ const Navbar = ({ type, pg, cartCount }) => {
 					const signIn = document.getElementById("sign-in");
 					const logOut = document.getElementById("log-out");
 					const myorders = document.getElementById("myOrders");
-					if (signIn) {
-						signIn.classList.add("hidden");
-					}
-					if (logOut) {
-						logOut.classList.remove("hidden");
-						if (myorders) myorders.classList.remove("hidden");
-					}
+
+					setShowSignIn(false);
+					setShowLogout(true);
+					setShowMyOrders(true);
 				} else {
-					const myorders = document.getElementById("myOrders");
 					localStorage.setItem("isUserLoggedOut", true);
-					if (myorders) myorders.classList.add("hidden");
+					setShowLogout(false);
+					setShowSignIn(true);
+					setShowMyOrders(false);
 				}
 			});
 	}, []);
 	return (
-		<nav className="shadow-md mt-2 mb-2 overflow-x-hidden flex items-center justify-between">
-			<div className="w-44  md:ml-1">
-				<Link to="/">
-					<Logo />
-				</Link>
-			</div>
-			{showLinks && (
-				<div
-					id="links"
-					className="ml-9 gap-2 md:gap-10 flex mr-10  text-sm md:text-base"
-				>
-					<p>Hello, {userName}!</p>
+		<>
+			<nav className="shadow-md mt-2 mb-2 overflow-x-hidden flex items-center justify-between">
+				<div className="w-44  md:ml-1">
+					<Link to="/">
+						<Logo />
+					</Link>
+				</div>
+				{showLinks && (
+					<div
+						id="links"
+						className="ml-9 gap-4 md:gap-10 flex mr-10  text-sm md:text-base"
+					>
+						<p>Hello, {userName}!</p>
 
-					<div className="flex gap-2">
-						<SearchIcon />
-						<Link to="/search">Search</Link>
-					</div>
-					<div className="flex gap-2">
-						<UserIcon />
-						<Link to="/sellerSignup">Become Seller</Link>
-					</div>
-					<div id="sign-in" className="flex gap-2">
-						<LogInIcon />
-						<a
-							href="#"
+						<div className="hidden md:flex gap-2">
+							<SearchIcon />
+							<Link to="/search">Search</Link>
+						</div>
+						<div className="hidden md:flex gap-2">
+							<UserIcon />
+							<Link to="/sellerSignup">Become Seller</Link>
+						</div>
+						{showSignIn && (
+							<div id="sign-in" className="hidden md:flex gap-2">
+								<LogInIcon />
+								<a
+									href="#"
+									onClick={() => {
+										setShowModal(true);
+									}}
+								>
+									Sign in
+								</a>
+							</div>
+						)}
+
+						{showMyOrders && (
+							<div id="myOrders" className="hidden md:flex gap-2 ">
+								<ThunderIcon />
+								<a href="/userOrders">My Orders</a>
+							</div>
+						)}
+
+						<div className="hidden md:flex gap-2">
+							<div className="relative flex">
+								<CartIcon />
+								{cartCount > 0 && (
+									<>
+										<span class="animate-ping absolute right-0 top-0 rounded-full bg-green-500 w-4 h-4 top right p-0 m-0 text-white font-mono text-sm  leading-tight text-center"></span>
+										<span class=" absolute right-0 top-0 rounded-full bg-green-500 w-4 h-4 top right p-0 m-0 text-white font-mono text-sm  leading-tight text-center">
+											{cartCount}
+										</span>
+									</>
+								)}
+							</div>
+							<a href="/checkout">Cart</a>
+						</div>
+						<div
 							onClick={() => {
-								setShowModal(true);
+								setMenuOpen(!menuOpen);
 							}}
+							className="cursor-pointer md:hidden flex gap-2"
 						>
-							Sign in
-						</a>
+							<HamBurgerIcon />
+						</div>
+						{showLogout && (
+							<div id="log-out" className="md:flex gap-2 hidden">
+								<LogOutIcon />
+								<a href="http://localhost:8000/logout">Logout</a>
+							</div>
+						)}
 					</div>
-					<div id="myOrders" className="flex gap-2 ">
-						<ThunderIcon />
-						<a href="/userOrders">My Orders</a>
-					</div>
-					<div className="flex gap-2">
-						<div className="relative flex">
-							<CartIcon />
-							{cartCount > 0 && (
+				)}
+				{!showLinks && (
+					<div className="ml-9 gap-2 md:gap-10 flex mr-10  text-sm md:text-base">
+						<div className="flex gap-2">
+							{!sellerLogin ? (
 								<>
-									<span class="animate-ping absolute right-0 top-0 rounded-full bg-green-500 w-4 h-4 top right p-0 m-0 text-white font-mono text-sm  leading-tight text-center"></span>
-									<span class=" absolute right-0 top-0 rounded-full bg-green-500 w-4 h-4 top right p-0 m-0 text-white font-mono text-sm  leading-tight text-center">
-										{cartCount}
-									</span>
+									<LogInIcon />
+									<a href="/sellerLogin">Seller Login</a>
+								</>
+							) : (
+								<>
+									<UserIcon />
+									<a href="/sellerSignup">Seller Signup</a>
 								</>
 							)}
 						</div>
-						<a href="/checkout">Cart</a>
 					</div>
-					<div id="log-out" className="flex gap-2 hidden">
-						<LogOutIcon />
-						<a href="http://localhost:8000/logout">Logout</a>
+				)}
+				{showModal && (
+					<Modal
+						title="Sign in"
+						showModal={showModal}
+						onChange={(value) => setShowModal(value)}
+					>
+						<a
+							href="http://localhost:8000/google"
+							className=" mt-5 mb-20 relative bottom-2  w-4/5 flex p-1 bg-blue-400 hover:bg-blue-500 transition ease-in-out duration-500 items-center h-18 md:h-20 text-xl shadow-lg rounded-md"
+						>
+							<div className=" bg-white  rounded-md p-3 flex justify-center items-center h-full overflow-hidden">
+								<img
+									src="https://img.icons8.com/color/48/000000/google-logo.png"
+									alt="google-png"
+								/>
+							</div>
+							<div className="text-white ml-10">Sign In</div>
+						</a>
+					</Modal>
+				)}
+			</nav>
+			<Menu
+				right
+				width={"50%"}
+				isOpen={menuOpen}
+				customBurgerIcon={false}
+				customCrossIcon={false}
+				className="md:hidden bg-gray-50 p-3"
+				overlayClassName="md:hidden"
+			>
+				<a className=" hover:bg-gray-100" href="/search">
+					<div className="flex gap-3">
+						<SearchIcon />
+						Search
 					</div>
-				</div>
-			)}
-			{!showLinks && (
-				<div className="ml-9 gap-2 md:gap-10 flex mr-10  text-sm md:text-base">
-					<div className="flex gap-2">
-						{!sellerLogin ? (
+				</a>
+				<a className="mt-3 hover:bg-gray-100" href="/sellerSignup">
+					<div className="flex gap-3">
+						<UserIcon />
+						Become Seller
+					</div>
+				</a>
+				{showSignIn && (
+					<a
+						className="mt-3 hover:bg-gray-100"
+						href="http://localhost:8000/google"
+					>
+						<div className="flex gap-3">
+							<LogInIcon />
+							Sign in
+						</div>
+					</a>
+				)}
+
+				{showLogout && (
+					<a
+						className="mt-3 hover:bg-gray-100"
+						href="http://localhost:8000/logout"
+					>
+						<div className="flex gap-3">
+							<LogOutIcon />
+							Logout
+						</div>
+					</a>
+				)}
+
+				{showMyOrders && (
+					<a className="mt-3 hover:bg-gray-100" href="/userOrders">
+						<div className="flex gap-3">
+							<ThunderIcon />
+							My Orders
+						</div>
+					</a>
+				)}
+
+				<a className="mt-3 hover:bg-gray-100" href="/checkout">
+					<div className="relative flex gap-3">
+						<CartIcon />
+						{cartCount > 0 && (
 							<>
-								<LogInIcon />
-								<a href="/sellerLogin">Seller Login</a>
-							</>
-						) : (
-							<>
-								<UserIcon />
-								<a href="/sellerSignup">Seller Signup</a>
+								<span class="animate-ping absolute left-3 top-0 rounded-full bg-green-500 w-4 h-4 top right p-0 m-0 text-white font-mono text-sm  leading-tight text-center"></span>
+								<span class=" absolute left-3 top-0 rounded-full bg-green-500 w-4 h-4 top right p-0 m-0 text-white font-mono text-sm  leading-tight text-center">
+									{cartCount}
+								</span>
 							</>
 						)}
+						Cart
 					</div>
-				</div>
-			)}
-			{showModal && (
-				<Modal
-					title="Sign in"
-					showModal={showModal}
-					onChange={(value) => setShowModal(value)}
-				>
-					<a
-						href="http://localhost:8000/google"
-						className=" mt-5 mb-20 relative bottom-2  w-4/5 flex p-1 bg-blue-400 hover:bg-blue-500 transition ease-in-out duration-500 items-center h-18 md:h-20 text-xl shadow-lg rounded-md"
-					>
-						<div className=" bg-white  rounded-md p-3 flex justify-center items-center h-full overflow-hidden">
-							<img
-								src="https://img.icons8.com/color/48/000000/google-logo.png"
-								alt="google-png"
-							/>
-						</div>
-						<div className="text-white ml-10">Sign In</div>
-					</a>
-				</Modal>
-			)}
-		</nav>
+				</a>
+			</Menu>
+		</>
 	);
 };
 
